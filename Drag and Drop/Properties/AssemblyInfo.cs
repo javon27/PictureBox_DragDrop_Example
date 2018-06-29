8456033903db20266d7323cd474710822cfcc07a
javon27 <jaysontrades@gmail.com>
2018-06-29 09:28:30 -0500

    Remove company info

--- Drag and Drop/Properties/AssemblyInfo.cs
+++ Drag and Drop/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Drag and Drop")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright © 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
